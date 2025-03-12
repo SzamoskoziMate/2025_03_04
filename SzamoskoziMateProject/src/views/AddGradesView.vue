dfcebc1a9fdc30d8af8dcdc665cb471db18c6e49
SzamoskoziMate <szamoskozi.mate@blathy.info>
2025-03-12 20:54:51 +0100

    second commit

--- SzamoskoziMateProject/src/views/AddGradesView.vue
+++ SzamoskoziMateProject/src/views/AddGradesView.vue
@@ -5,7 +5,13 @@ export default{
 </script>
 
 <template>
-  
+  <main class="m-auto p-3 container">
+    <div class="row">
+      <div class="col">
+        <h1>Új Jegy Hozzáadása</h1>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style>
--- SzamoskoziMateProject/src/views/StudentView.vue
+++ SzamoskoziMateProject/src/views/StudentView.vue
@@ -1,11 +1,39 @@
 <script>
-export default{
-    name: 'StudentView'
+import {http} from "@/utils/http.js";
+import {RouterLink} from "vue-router";
+export default {
+  name: "StudentView",
+  components:{
+    RouterLink
+  },
+  data(){
+    return{
+      student: {}
+    }
+  },
+  methods:{
+    async getStudentData(){
+      const response = await http.get(`student/${this.$route.params.id}`)
+      this.student = await response.data.data;
+    }
+  },
+  mounted() {
+    this.getStudentData();
+  }
 }
 </script>
 
+
+
 <template>
-  
+  <section class="container" v-if="student">
+    <h1>{{student.name}}</h1>
+    <hr>
+    <ul class="list-group">
+      <li class="list-group-item list-group-item-info">Azonosító: {{student.eduid}}</li>
+      <li class="list-group-item list-group-item-danger">Jegy: {{drink.price}}Ft</li>
+    </ul>
+  </section>
 </template>
 
 <style>
